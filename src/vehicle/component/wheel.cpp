35a51b7e22f2a36991beee492b3adbb0fae30c37
yancytong <yancytong@tencent.com>
2025-11-03 15:30:22 +0800

    251103: wheel

--- src/vehicle/component/wheel.cpp
+++ src/vehicle/component/wheel.cpp
@@ -1,1 +1,30 @@
 #include "wheel.h"
+#include "physics/shape/sphere_shape.h"
+#include "physics/shape/capsule_shape.h"
+#include "utils/logger.h"
+
+namespace pe_vehicle {
+
+Wheel::Wheel(WheelType type, pe::Real radius, pe::Real width, pe::Real mass, pe::Real friction):
+    _radius(radius), _width(width), _mass(mass), _friction(friction) {
+    _wheel = new pe_physics_object::RigidBody();
+    _wheel->setMass(mass);
+    _wheel->setFrictionCoeff(friction);
+
+    pe_physics_shape::Shape* shape = nullptr;
+    switch (type) {
+        case WheelType::WT_Sphere:
+            shape = new pe_physics_shape::SphereShape(radius);
+            break;
+        case WheelType::WT_Capsule:
+            shape = new pe_physics_shape::CapsuleShape(radius, width);
+            break;
+        case WheelType::WT_Cylinder:
+            // not implemented yet
+            PE_LOG_ERROR << "Cylinder wheel shape is not implemented yet." << PE_ENDL;
+            break;
+    }
+    _wheel->setCollisionShape(shape);
+}
+
+} // namespace pe_vehicle
--- src/vehicle/component/wheel.h
+++ src/vehicle/component/wheel.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include "physics/physics.h"
+#include "physics/object/rigidbody.h"
+
+namespace pe_vehicle {
+
+enum class WheelType {
+    WT_Sphere,
+    WT_Capsule,
+    WT_Cylinder
+};
+
+class Wheel {
+    COMMON_MEMBER_GET(pe::Real, radius, Radius)
+    COMMON_MEMBER_GET(pe::Real, width, Width)
+    COMMON_MEMBER_GET(pe::Real, mass, Mass)
+    COMMON_MEMBER_GET(pe::Real, friction, Friction)
+
+protected:
+    pe_physics_object::RigidBody* _wheel = nullptr;
+
+public:
+    Wheel() = delete;
+    Wheel(WheelType type, pe::Real radius, pe::Real width, pe::Real mass, pe::Real friction);
+    virtual ~Wheel();
+};
+
+} // namespace pe_vehicle
